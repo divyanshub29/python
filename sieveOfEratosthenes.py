0d726aa319bb5b733e50e074345e2288b120062d
algolover <rockin.divyanshu@gmail.com>
2016-06-16 05:40:32 +0530

    Added Sieve Of Eratosthenes for Prime Numbers

---
+++ sieveOfEratosthenes.py
@@ -0,0 +1,17 @@
+   
+from math import sqrt
+
+def sieveOfEratosthenes(n):
+    primes = range(3, n + 1, 2) # primes above 2 must be odd so start at three and increase by 2
+    for base in xrange(len(primes)):
+        if primes[base] is None:
+            continue
+        if primes[base] >= sqrt(n): # stop at sqrt of n
+            break
+
+        for i in xrange(base + (base + 1) * primes[base], len(primes), primes[base]):
+            primes[i] = None
+    primes.insert(0,2)
+    return filter(None, primes)
+
+arr=((sieveOfEratosthenes(104752)))#change the number as per requirement
