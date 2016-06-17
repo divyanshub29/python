a74b2b7c9ff25eaffc54461bf31cdec433b3a4f4
algolover <rockin.divyanshu@gmail.com>
2016-06-17 11:23:01 +0530

    Fermat's Little Theorem-Pobabilistic Primality Test

---
+++ fermat little theorem.py
@@ -0,0 +1,43 @@
+from random import *
+
+def carmichael(n):
+    arr=[561,1105,1729,2465,2821,6601,8911,41041,62745,63973,75361,101101,126217,172081,188461,278545,340561]
+    if n in arr:
+        return True
+
+    return False
+
+
+def fermat(n,k):
+
+
+    if n==2 or n==3:
+        return True
+
+    if n%2==0 or n<=1:
+        return False
+
+    if carmichael(n)==True:
+        return False
+    
+    for i in xrange(k):
+        a=randint(2,n-1)
+
+
+        if pow(a,n-1,n) != 1:
+            return False
+
+    return True
+
+
+print fermat(11,3)
+print fermat(561,1)
+
+
+'''there exist some composite numbers which passes the fermat's test.
+    Such numbers ar called Carmichael numbers.
+
+    some of the carmichael numbers are:
+    [561,1105,1729,2465,2821,6601,8911,41041,62745,63973,75361,101101,126217,172081,188461,278545,340561]
+'''
+
