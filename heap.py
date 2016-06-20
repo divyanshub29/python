73ccda6666bb33577686a7c0dc22eff309f3ec47
algolover <rockin.divyanshu@gmail.com>
2016-06-20 13:46:59 +0530

    added kthsmallest number fucntion

--- heap.py
+++ heap.py
@@ -3,6 +3,9 @@ def buildHeap(a):
     for i in range(n/2-1,-1,-1):
         heapify(a,i,n)
 
+
+
+'''heapify make sure that parent node is larger than both children and if it is not then it perform swapping'''
 def heapify(a,idx,maxIdx):
     left=2*idx+1
     right=2*idx+2
@@ -26,6 +29,16 @@ def heapsort(a):
         heapify(a,0,i)
 a=[4,3,2,6,8,9,1,12,43,32]
 
-heapsort(a)
 
-print a
+def kthsmallest(collection,k):
+    '''Retrun kth smallest element in collection for valid k >=1'''
+    a=collection[:k]
+    buildHeap(a)
+
+    for i in range(k,len(collection)):
+        if collection[i]<a[0]:
+            a[0]=collection[i]
+            heapify(a,0,k)
+    return a[0]
+
+print kthsmallest(a,3)
