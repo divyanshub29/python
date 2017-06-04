efe92f056eebf0e42ba89d01443481c236a3b630
algolover <rockin.divyanshu@gmail.com>
2017-06-05 00:34:26 +0530

    added function which tracks start index and end index of maximum

--- Maximum subarray problem.py
+++ Maximum subarray problem.py
@@ -7,4 +7,34 @@ def max_subarray(A):
     return max_so_far
 
 
-print max_subarray(A)
+#print max_subarray(A)
+
+def kadane(arr):
+    a=b=0
+    for x in arr:
+        a=max(0,a+x)
+        b=max(b,a)
+        print a,b
+    return b
+
+
+# Print Max subarray with Start and End indices
+
+arr=[-2, -3, 4, -1, -2, 1, 5, -3]
+a=0
+b=arr[0]
+start=end=s=0
+for i,x in enumerate(arr):
+    a+=x
+    if b<a:
+        b=a
+        start=s
+        end=i
+    if a<0:
+        a=0
+        s=i+1
+
+
+
+print b
+print start,end
