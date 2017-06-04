b11c6500af29cbd4ff3742d99876b35c47f31b34
algolover <rockin.divyanshu@gmail.com>
2017-06-05 00:52:46 +0530

    modified set_bits

--- set_bits.py
+++ set_bits.py
@@ -2,7 +2,7 @@
 
 """ COUNTS NUMBER OF 1s IN BINARY REPRESENTATION OF A NUMBER"""
 """ ALGORITHM USED IS BRIAN KERNIGHAN ALGO"""
-""" TIME COMPLEXITY IS Log(N)"""
+""" TIME COMPLEXITY IS Log(N)in worst case,otherwise O(k) where k is number of set bits"""
 def count_set_bits(n):
     count=0
     while n!=0:
@@ -10,3 +10,33 @@ def count_set_bits(n):
         count=count+1
     return count
     
+
+'''====check if the ith bit is set or not===='''
+
+def checkbit(n,i):
+    if(n&(1<<i)):
+        return True
+    return False
+
+
+'''====generate all possible subset of a set==='''
+
+def genSubsets(a):
+    a=list(set(a))
+    n=len(a)
+    for i in range(2**n):
+        for j in range(n):
+            if i & (1<<j):
+                print a[j],
+        print ''
+
+'''===largest power of 2 which is less than or equal to the given number N.==='''
+
+def power2(n):
+    n=n|(n>>1)
+    n=n|(n>>2)
+    n=n|(n>>4)
+    n=n|(n>>8)
+
+    return (n+1)>>1
+
