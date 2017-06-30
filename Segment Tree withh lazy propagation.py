6ea73d5f555163c1f3a0ed9a400224091f327037
Divyanshu Bansal <rockin.divyanshu@gmail.com>
2017-06-30 10:20:44 +0530

    Improved Readability, Added Comments and Bug Fixes

--- Segment Tree withh lazy propagation.py
+++ Segment Tree withh lazy propagation.py
@@ -1,8 +1,16 @@
+# Input Array
 arr=map(int,raw_input().split())
+
+# Length of input array
 n=len(arr)
+
+# Segment Tree, in worst case it requires 4*n memory.
 seg=[0]*(4*n+2)
+
+# Lazy array, initialized with all zeroes as seg is also 0 inititalized
 lazy=list(seg)
 
+# Function to Build Segment Tree
 def build(node,start,end):
     if start==end:
         seg[node]=arr[start]
@@ -12,9 +20,14 @@ def build(node,start,end):
         build(2*node+1,mid+1,end)
         seg[node]=seg[2*node]+seg[2*node+1]
 
+        
+# Here "val" is the difference between old and current value
+# For eg. if old value was 3 and new val is 5 then val is (5-3) =2
+# idx is index int he array to be updated.
+# This update function doesn't use lazy Propagation.
 def update(node,start,end,idx,val):
     if start==end:
-        a[idx]+=val
+        # arr[idx]+=val
         seg[node]+=val
     else:
         mid=(start+end)/2
@@ -24,7 +37,9 @@ def update(node,start,end,idx,val):
             update(2*node+1,mid+1,end,idx,val)
         seg[node]=seg[2*node]+seg[2*node+1]
 
+        
 # query in range l to r
+# This query function doesn't use Lazy Propagation.
 def query(node,start,end,l,r):
     if r<start or end<l:
         return 0
@@ -36,6 +51,8 @@ def query(node,start,end,l,r):
     p2=query(2*node+1,mid+1,end,l,r)
     return p1+p2
 
+
+# This Range Update function doesn't use Lazy Propagation.
 def update_range(node,start,end,l,r,val):
     if start>end or start>r or end<l:
         return
@@ -47,6 +64,14 @@ def update_range(node,start,end,l,r,val):
     update_range(node*2+1,mid+1,end,l,r,val)
     seg[node]=seg[node*2]+seg[node*2+1]
 
+
+""" ------------- LAZY PROPAGATION--------------"""    
+
+# Instead of using query and update functions written above,
+# these lazy functions can be used for better performance
+
+# Lazy Update function, efficient than simple update function
+# For point update purpose, pass same value to l and r i.e. l==r
 def lazy_update(node,start,end,l,r,val):
     if lazy[node]!=0:
         seg[node]+=(end-start+1)*lazy[node]
@@ -69,6 +94,7 @@ def lazy_update(node,start,end,l,r,val):
     lazy_update(node*2+1,mid+1,end,l,r,val)
     seg[node]=seg[node*2]+seg[node*2+1]
 
+# Lazy Query function
 def lazy_query(node,start,end,l,r):
     if start>end or start>r or end<l:
         return 0
@@ -84,7 +110,8 @@ def lazy_query(node,start,end,l,r):
         return seg[node]
     mid=(start+end)/2
     p1=lazy_query(node*2,start,mid,l,r)
-    p2=lazy_query(mode*2,mid+1,end,l,r)
+    p2=lazy_query(node*2+1,mid+1,end,l,r)
     return p1+p2
 
-    
+
+
